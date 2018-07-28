efae0b42b83422b9532cc0b21709d65f083c8f6f
Kitall E-Commerce <kitall.ecomm@gmail.com>
2018-07-27 21:12:20 -0300

    Added code for slider

--- js/slider.js
+++ js/slider.js
@@ -1,43 +1,43 @@
 jQuery(document).ready(function ($) {
 
-	$('#checkbox').change(function () {
+	$('.checkbox').change(function () {
 		setInterval(function () {
 			moveRight();
 		}, 3000);
 	});
 
-	var slideCount = $('#slider ul li').length;
-	var slideWidth = $('#slider ul li').width();
-	var slideHeight = $('#slider ul li').height();
+	var slideCount = $('.slider ul li').length;
+	var slideWidth = $('.slider ul li').width();
+	var slideHeight = $('.slider ul li').height();
 	var sliderUlWidth = slideCount * slideWidth;
 
-	$('#slider').css({
+	$('.slider').css({
 		width: slideWidth,
 		height: slideHeight
 	});
 
-	$('#slider ul').css({
+	$('.slider ul').css({
 		width: sliderUlWidth,
 		marginLeft: -slideWidth
 	});
 
-	$('#slider ul li:last-child').prependTo('#slider ul');
+	$('.slider ul li:last-child').prependTo('.slider ul');
 
 	function moveLeft() {
-		$('#slider ul').animate({
+		$('.slider ul').animate({
 			left: +slideWidth
 		}, 200, function () {
-			$('#slider ul li:last-child').prependTo('#slider ul');
-			$('#slider ul').css('left', '');
+			$('.slider ul li:last-child').prependTo('.slider ul');
+			$('.slider ul').css('left', '');
 		});
 	};
 
 	function moveRight() {
-		$('#slider ul').animate({
+		$('.slider ul').animate({
 			left: -slideWidth
 		}, 200, function () {
-			$('#slider ul li:first-child').appendTo('#slider ul');
-			$('#slider ul').css('left', '');
+			$('.slider ul li:first-child').appendTo('.slider ul');
+			$('.slider ul').css('left', '');
 		});
 	};
 
